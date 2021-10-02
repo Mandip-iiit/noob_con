6e2b75b1f433510a9159c72cd3bc0a1e558019d6
Shubhit Sardana <91517488+Critical07@users.noreply.github.com>
2021-10-02 19:47:42 +0530

    Update 12_tuples.py

--- 12_tuples.py
+++ 12_tuples.py
@@ -1,8 +1,12 @@
+# This is a tuple
 t = (1, 2, 3, 4, 5, 6)
-
+# here we print this tuple
 print(t)
 
-# t[1]=34
-t1 = (2,)
+# since we know that tuple is immutable or in other words it cannot be changed
+# we can see here that if we try to change it
+t[1] = 2
+print(t)
 
-print(t1)
+#it throws an error that - 'tuple' object does not support item assignment... 
+# so ther conclusion is that we cannot change tuple
